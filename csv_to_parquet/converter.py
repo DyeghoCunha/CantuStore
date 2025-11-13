006efee2c58564d6d08aa9afcb8bd8a2a9ab6bb2
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2025-11-13 16:37:06 -0300

    fix(script): Força dtypes e desabilita low_memory para carregar CSVs problemáticos

--- csv_to_parquet/converter.py
+++ csv_to_parquet/converter.py
@@ -2,18 +2,25 @@ import pandas as pd
 import os
 
 arquivos_csv = [
-    "tb_cmssitelp.csv",
-    "tb_paymentmodes.csv",
-    "tb_regions.csv",
     "tb_users.csv"
 ]
+tipos_de_dados = {
+    "p_dddphone": str,
+    "p_phone": str,
+    "p_originaluid": str,
+    "p_uid": str,
+    "hjmpTS": str,
+    "createdTS": str,
+    "p_dddphone":str,
+    "p_dddcellphoneaditional":str
 
+}
 print("Iniciando a conversão de CSV para Parquet...")
 
 for arquivo in arquivos_csv:
     try:
         novo_nome = arquivo.replace(".csv", ".parquet")
-        df = pd.read_csv(arquivo,sep='|')
+        df = pd.read_csv(arquivo,sep='|', dtype=tipos_de_dados, low_memory=False)
         df.to_parquet(novo_nome, index=False)
         print(f"Arquivo convertido com sucesso:{arquivo} -> {novo_nome}")
     except Exception as e:
