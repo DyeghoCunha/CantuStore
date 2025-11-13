3a2fb94ca3439b3663347aa4f4dbeac02c0dcf31
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2025-11-13 15:31:40 -0300

    feat(scripts): Adiciona script Python para converter CSV para Parquet localmente

---
+++ csv_to_parquet/converter.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+arquivos_csv = [
+    "tb_cmssitelp.csv",
+    "tb_paymentmodes.csv",
+    "tb_regions.csv",
+    "tb_users.csv"
+]
+
+print("Iniciando a conversão de CSV para Parquet...")
+
+for arquivo in arquivos_csv:
+    try:
+        novo_nome = arquivo.replace(".csv", ".parquet")
+        df = pd.read_csv(arquivo)
+        df.to_parquet(novo_nome, index=False)
+        print(f"Arquivo convertido com sucesso:{arquivo} -> {novo_nome}")
+    except Exception as e:
+        print(f"Erro ao converter o arquivo {arquivo}: {e}")
+print("Conversão concluída.")
